db99d1b011aa90a1ba79c872e0cd224b95b0007c
patia141 <patia141@gmail.com>
2024-01-29 13:28:10 +0100

    test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
-﻿using System;
+﻿using Projekt_system_zarzadzania_kursami_na_uczelni;
+using System;
 using System.Collections.Generic;
+
+namespace System_zarzadzania_kursami_na_uczelni
+{
+    class Program
+    {
+        static void Main()
+        {
+            //studenci
+            Student student1 = new Student { Imie = "Jan", Nazwisko = "Kowalski" };
+            student1.DodajStudenta(student1);
+
+            Student student2 = new Student { Imie = "Patrycja", Nazwisko = "Lipa" };
+            student2.DodajStudenta(student2);
+
+            Student student3 = new Student { Imie = "Karolina", Nazwisko = "Karp" };
+            student3.DodajStudenta(student3);
+
+            Student student4 = new Student { Imie = "Tomasz", Nazwisko = "Jezioro" };
+            student4.DodajStudenta(student4);
+
+            student2.UsunStudenta(student2.StudentId);
+
+            Student studentToUpdate = new Student { StudentId = 1, Imie = "Janusz", Nazwisko = "Nowak" };
+            student1.AktualizujStudenta(studentToUpdate);
+
+            Student studentDoZnalezienia = student2.PobierzStudenta(2);
+            if (studentDoZnalezienia != null)
+            {
+                Console.WriteLine($"\nStudent o ID {studentDoZnalezienia.StudentId} nadal znajduje się na liście studentów");
+            }
+            else
+            {
+                Console.WriteLine("\nNie znaleziono studenta o podanym ID");
+            }
+
+            Console.WriteLine("Studenci:");
+            foreach (var student in Student.studenci)
+            {
+                Console.WriteLine($"ID: s{student.StudentId}, Imię i nazwisko: {student.Imie} {student.Nazwisko}");
+            }
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_system_zarzadzania_kursami_na_uczelni
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+
+        public static List<Student> studenci = new List<Student>();
+
+        public void DodajStudenta(Student student)
+        {
+            student.StudentId = studenci.Count + 1;
+            studenci.Add(student);
+        }
+
+        public Student PobierzStudenta(int studentId)
+        {
+            return studenci.Find(s => s.StudentId == studentId);
+        }
+
+        public void AktualizujStudenta(Student updatedStudent)
+        {
+            var existingStudent = studenci.Find(s => s.StudentId == updatedStudent.StudentId);
+            if (existingStudent != null)
+            {
+                existingStudent.Imie = updatedStudent.Imie;
+                existingStudent.Nazwisko = updatedStudent.Nazwisko;
+            }
+        }
+
+        public void UsunStudenta(int studentId)
+        {
+            studenci.RemoveAll(s => s.StudentId == studentId);
+        }
+    }
+}
